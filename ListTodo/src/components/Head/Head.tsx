401fa8bb319795cf0820880ea245016dbb19aa93
tudou <642783485@qq.com>
2023-04-09 22:25:46 +0800

    添加prettier

--- ListTodo/src/components/Head/Head.tsx
+++ ListTodo/src/components/Head/Head.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import logo from '../../logo.svg'
 import './Head.scss';
 export default function Head() {
   return (
     <div className="head_box">
       <div className='logo'>
         todo
+        <img src={logo} alt="" />
       </div>
       <div className='head_item'>
         <span>隐藏</span>
---
+++ readme.md
@@ -0,0 +1,6 @@
+技术选型
+electron 
+react^18.2.0 
+ts
+vite^4.2.0
+prettier
